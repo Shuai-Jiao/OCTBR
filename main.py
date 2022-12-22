d719556be2ff7c24b4b3ec3651332eee4c51eb54
Shuai-Jiao <jiao.shuai.1998.12.01@outlook.com>
2022-12-22 13:43:31 +0100

    Construct flower model and restricted model

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import matplotlib
 import pm4py
 import os
@@ -14,6 +13,10 @@ from pm4py.visualization.footprints import visualizer as fp_visualizer
 from pm4py.algo.discovery.footprints import algorithm as footprints_discovery
 import datetime
 import pandas as pd
+from pm4py.objects.petri_net.obj import PetriNet, Marking
+from pm4py.objects.petri_net.utils import petri_utils
+import pm4py
+from pm4py.visualization.common import gview
 
 
 def ParsingCSV(csvpath, parameters=None):
@@ -110,6 +113,8 @@ def MergeOCFM(ocfmlist,variablity = False):
                 elif (a2, a1) in ocfm["sequence"]:
                     symb_1 = PREV_SYMBOL
                     conflict = False
+                else:
+                    continue
                 symb = symb_1+"<sup>"+ot+"</sup>"
                 relation += (symb+',')
             if conflict == True:
@@ -132,9 +137,9 @@ def MergeOCFM(ocfmlist,variablity = False):
 
 def EvalOCFM(logocfm,modelocfm): #ocfm1 and ocfm2 are both lists
     nonconflicttotal, conform, _ = CompareOCFM(logocfm,modelocfm)
-    precision = conform/nonconflicttotal
-    nonconflicttotal, conform, seqratio = CompareOCFM(modelocfm,logocfm)
     fitness = conform/nonconflicttotal
+    nonconflicttotal, conform, seqratio = CompareOCFM(modelocfm,logocfm)
+    precision = conform/nonconflicttotal
     simplicity = 1-1/(1 + np.exp(-10*seqratio+7)) #offset to 7
     return fitness, precision, simplicity 
         
@@ -161,7 +166,16 @@ def CompareOCFM(conformed,conforming): # parameters are ocfm
         for pair1 in ocfm1['parallel']:
             for pair2 in conforming[ot1]['parallel']:
                 if pair1 == pair2:
-                    conform += 1   
+                    conform += 1
+            
+            for pair2 in conforming[ot1]['sequence']:
+                if pair1[0] == pair1[1]:
+                    if pair1 == pair2:
+                        conform += 1
+                else:
+                    if pair1 == pair2:
+                        conform += 0.5
+            conflictele = list(set(conflictele)-set([pair1]))
                     
         nonconflicttotal += (len(ocfm1['sequence'])+len(ocfm1['parallel']))           
         seq += len(ocfm1['sequence'])    
@@ -169,6 +183,7 @@ def CompareOCFM(conformed,conforming): # parameters are ocfm
     return nonconflicttotal, conform, seq/(nonconflicttotal+len(conflictele))
 
 def EvalbyOCFM(ocel,parameters=None):
+    '''Suspended work'''
     if parameters == None:
         ocpn = pm4py.discover_oc_petri_net(ocel)
         ocpnlist = decomposeOCPN(ocpn)
@@ -179,27 +194,90 @@ def EvalbyOCFM(ocel,parameters=None):
         raise ValueError("Parameter configuration is not done so far")
 
 
-def Evaluation(ocel):
-    ocpn = pm4py.discover_oc_petri_net(ocel)
+def Evaluation(ocel,ocpn):
     ocpnlist = decomposeOCPN(ocpn)
     ocfmmodel = OCPN2OCFM(ocpnlist)
     ocfmlog = OCEL2OCFM(ocel)
+    gviz1=MergeOCFM(ocfmmodel)
+    gviz2=MergeOCFM(ocfmlog)
+    gview.view(gviz1)
+    gview.view(gviz2)
     result = EvalOCFM(ocfmlog,ocfmmodel)
     return result
 
+def Flowermodel(model):
+    ocpn = model
+    activitytype = {}
+    for act in model['activities']:
+        activitytype[act]=[]
+        for ot in model['object_types']:
+            for ele in list(model['petri_nets'][ot][0].transitions):
+                if act==ele.label:
+                    activitytype[act].append(ot)
+    objecttype = model['object_types']
+    
+    places = {}
+    transitions = {}
+    im,fm = Marking(),Marking()
+    for ot in objecttype:
+        net = PetriNet(ot)
+        places[ot] = PetriNet.Place(ot)
+        net.places.add(places[ot])
+        im[places[ot]] = 1
+        fm[places[ot]] = 1
+        
+        for trans in activitytype.keys():
+            for ot2 in activitytype[trans]: 
+                if ot2 == ot:
+                    transitions[trans] = PetriNet.Transition(trans,trans)
+                    net.transitions.add(transitions[trans])
+                    petri_utils.add_arc_from_to(places[ot],transitions[trans],net)
+                    petri_utils.add_arc_from_to(transitions[trans],places[ot],net)    
+        ocpn['petri_nets'][ot] = [net,im,fm]
+    return ocpn
+
+def Restrictedmodel(model,ocel):
+    for ot in model['object_types']:
+        flat_log = pm4py.ocel_flattening(ocel, ot)
+        onetracelog = flat_log.loc[flat_log['case:concept:name'] == flat_log['case:concept:name'][0]]
+        event_log = pm4py.convert_to_event_log(onetracelog)
+        net, im, fm = pm4py.discover_petri_net_inductive(event_log)
+        model['petri_nets'][ot] = (net,im,fm)
+    return model
+
+
+
 if __name__ == "__main__":
-    path1 = './OCEL/running-example.jsonocel'
+    path1 = '/Users/jiao.shuai.1998.12.01outlook.com/Documents/OCFM/OCEL/jsonocel/running-example.jsonocel'
     ocel1 = pm4py.read_ocel(path1)
-    path2 = './OCEL/example.csv'
+    path2 = '/Users/jiao.shuai.1998.12.01outlook.com/Documents/OCFM/OCEL/example.csv'
 
     parameter1 = {'time_name':'event_timestamp','act_name':'event_activity','object_type':['order','item','delivery']}
-    ParsingCSV(path2,parameter1).to_csv('./OCEL/ocelexample.csv')
-    ocel2 = pm4py.read_ocel('./OCEL/ocelexample.csv')
+    ParsingCSV(path2,parameter1).to_csv('./ocelexample.csv')
+    ocel2 = pm4py.read_ocel('./ocelexample.csv')
 
-    result1 = Evaluation(ocel1)
-    result2 = Evaluation(ocel2)
-    
-    for ele in [(path1,result1), (path2,result2)]:
+    ocpn1 = pm4py.discover_oc_petri_net(ocel1)
+    ocpn2 = pm4py.discover_oc_petri_net(ocel2)
+    flower1 = Flowermodel(ocpn1)
+    restricted1 = Restrictedmodel(ocpn1,ocel1)
+    flower2 = Flowermodel(ocpn2)
+    restricted2 = Restrictedmodel(ocpn2,ocel2)
+    #pm4py.view_ocpn(ocpn1, format="png")
+    #pm4py.view_ocpn(flower1, format="png")
+
+    result1 = Evaluation(ocel1,ocpn1)
+    result2 = Evaluation(ocel2,ocpn2)
+    result3 = Evaluation(ocel1,flower1)
+    result4 = Evaluation(ocel2,flower2)
+    result5 = Evaluation(ocel1,restricted1)
+    result6 = Evaluation(ocel2,restricted2)
+
+    #list1 = [(path1,result1), (path2,result2),(path1,result3),(path2,result4)]
+    list2 = [(path1,result1), (path1,result3),(path1,result5)]
+    list3 = [(path2,result2), (path2,result4),(path2,result6)]
+
+    for ele in list2+list3:
         print('The quality dimensions of',ele[0][7:],'compared to its discovered model are: ','\nThe fitness is: ',ele[1][0],'\nThe precision is: ',ele[1][1],'\nThe simplicity is: ',ele[1][2])
+
 
   
