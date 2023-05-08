23b19d13178ac085fef977c56094a70eb53d0412
Shuai-Jiao <jiao.shuai.1998.12.01@outlook.com>
2023-05-08 20:10:16 +0200

    Change the repository name

--- test/test1.py
+++ test/test1.py
@@ -3,7 +3,7 @@ sys.path.append(".")
 import visualization
 
 prefix = "/Users/jiao.shuai.1998.12.01outlook.com/Documents/OCFM/sample_logs/"
-ocellist = ["jsonocel/order_process.jsonocel"]
-#ocellist = ["jsonocel/order_process.jsonocel","jsonocel/p2p-normal.jsonocel"]
+#ocellist = ["jsonocel/order_process.jsonocel"]
+ocellist = ["jsonocel/order_process.jsonocel","jsonocel/p2p-normal.jsonocel"]
 #ocellist = ["csv/BPI2017-Final.csv"]
 print(visualization.Drawcomparisontable([prefix+str for str in ocellist],[],automodel=True))
